fe6da938242b040cbcf3e489bbb37a5b2794d9a0
Aditya_Thakare <adityathakare72@gmail.com>
2024-12-26 02:58:19 +0530

    initial blank commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mini mlops project dagshub mlflow s3',
+    author='aditya',
+    license='',
+)
